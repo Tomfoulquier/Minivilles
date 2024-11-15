31f01d321546a58ca42eb34471112bc46b82f730
GUIlhem <guilhem.lesaint.marquet@gmail.com>
2024-11-15 15:03:07 +0100

    Game Update

--- Minivilles/Game.cs
+++ Minivilles/Game.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace Minivilles
 {
+
     public class Game
     {
+
+        public List<Player> listJoueurs;
+        public List<int> stockCards;
+        public Pile referenceCards;
+        public bool isPlayerTurn;
+        public bool endGame;
+
+        public Game()
+        {
+
+            listJoueurs.Add(new Player())
+            listJoueurs.Add(new Player())
+
+            for (int i = 0; i < 8; i++) 
+            {
+
+                stockCards.Add(6);
+
+            }
+
+            referenceCards = new Pile();
+            // A FAIRE : Ajouter les Cards a Pile
+
+            isPlayerTurn = true;
+            endGame = false;
+
+        }
+
     }
+
 }
