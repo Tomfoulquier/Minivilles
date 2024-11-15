643618ea52b90cfd8f1c545cf6b72c028f1c534c
GUIlhem <guilhem.lesaint.marquet@gmail.com>
2024-11-15 17:19:12 +0100

    Update GAME

--- Minivilles/Game.cs
+++ Minivilles/Game.cs
@@ -17,6 +17,7 @@ namespace Minivilles
         public bool isPlayerTurn;
         public bool endGame;
         public Die die;
+        public Random random = new Random;
 
         public Game()
         {
@@ -39,20 +40,23 @@ namespace Minivilles
             }
 
             referenceCards = new Pile();
-            referenceCards.AddCard(new Card(1, "01", 2, "Champs de blé"));
-            referenceCards.AddCard(new Card(1, "01", 2, "Ferme"));
-            referenceCards.AddCard(new Card(2, "02", 1, "Boulangerie"));
-            referenceCards.AddCard(new Card(3, "11", 2, "Café"));
-            referenceCards.AddCard(new Card(4, "03", 2, "Superette"));
-            referenceCards.AddCard(new Card(5, "01", 2, "Forêt"));
-            referenceCards.AddCard(new Card(5, "12", 4, "Restaurant"));
-            referenceCards.AddCard(new Card(6, "04", 6, "Stade"));
+            referenceCards.AddCard(new Card(1, "0,1", 2, "Champs de blé"));
+            referenceCards.AddCard(new Card(1, "0,1", 2, "Ferme"));
+            referenceCards.AddCard(new Card(2, "0,2", 1, "Boulangerie"));
+            referenceCards.AddCard(new Card(3, "1,1", 2, "Café"));
+            referenceCards.AddCard(new Card(4, "0,3", 2, "Superette"));
+            referenceCards.AddCard(new Card(5, "0,1", 2, "Forêt"));
+            referenceCards.AddCard(new Card(5, "1,2", 4, "Restaurant"));
+            referenceCards.AddCard(new Card(6, "0,4", 6, "Stade"));
 
             isPlayerTurn = true;
             endGame = false;
 
-            listJoueurs[0].playerCity.AddCard(referenceCards[0]);
-            listJoueurs[0].playerCity.AddCard(referenceCards[2]);
+            listJoueurs[0].playerCity.AddCard(referenceCards.listCards[0]);
+            listJoueurs[0].playerCity.AddCard(referenceCards.listCards[2]);
+
+            listJoueurs[1].playerCity.AddCard(referenceCards.listCards[0]);
+            listJoueurs[1].playerCity.AddCard(referenceCards.listCards[2]);
 
             while (!endGame) 
             {
@@ -62,15 +66,54 @@ namespace Minivilles
                 if (isPlayerTurn) 
                 {
 
+                    Console.Clear();
+                    Console.WriteLine("Appuyez sur entrer pour lancer le dés");
+                    Console.ReadLine();
+
                     resultDie = die.RollDie(false);
 
+                    Console.Clear();
+                    Console.WriteLine("Vous avez tirer le {0}", resultDie);
+
+                    foreach (Card card in referenceCards.listCards) 
+                    {
+                    
+                        if(card.activationNumber == resultDie) 
+                        {
+
+                            ApplyEffect(0, card.effect);
+
+                        }
+                    
+                    }
+
+                    Console.WriteLine("piece j : " + listJoueurs[0].coins);
+                    Console.WriteLine("piece o : " + listJoueurs[1].coins);
 
                     isPlayerTurn = false;
+
+                    Console.ReadLine();
                 
                 }
                 else 
                 {
-                
+
+                    if (random.Next(0, 2)) 
+                    {
+                    
+                        for (int i = referenceCards.listCards.Count-1; i >= 0; i--) 
+                        {
+
+                            if (stockCards[i] > 0 && listJoueurs[1].coins >= referenceCards.listCards[i].price) 
+                            {
+
+
+                            
+                            }
+                        
+                        }
+                    
+                    }
                     
                     isPlayerTurn = true;
                 
@@ -87,9 +130,24 @@ namespace Minivilles
 
             if(indexPlayer == 0) {  inverseIndexPlayer = 1; } else { inverseIndexPlayer = 0; }
 
-            listJoueurs[indexPlayer].coins += (int.Parse(_effect.Split("")[1]));
+            listJoueurs[indexPlayer].coins += int.Parse(_effect.Split(",")[1]);
             
-            if (_effect.Split("")[0] == "1") { listJoueurs[inverseIndexPlayer].coins -= int.Parse(_effect.Split("")[1]);  }
+            if (_effect.Split(",")[0] == "1") { listJoueurs[inverseIndexPlayer].coins -= int.Parse(_effect.Split(",")[1]);  }
+
+            if(indexPlayer == 0) 
+            {
+
+                Console.WriteLine("Vous avez gagné {0} pieces", int.Parse(_effect.Split(",")[1]));
+            
+            }
+
+            if(inverseIndexPlayer == 0 && _effect.Split(",")[0] == "1") 
+            {
+
+                Console.WriteLine("Vous avez perdu {0} pieces", int.Parse(_effect.Split(",")[1]));
+
+            }
+            
 
 
         }
