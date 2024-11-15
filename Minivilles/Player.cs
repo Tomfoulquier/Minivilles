780a301ca901ad1fa386aa6b4bf73c953da339bb
Luka <grossmanluka52@gmail.com>
2024-11-15 16:04:42 +0100

    update player

--- Minivilles/Player.cs
+++ Minivilles/Player.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +11,27 @@ namespace Minivilles
 {
     public class Player
     {
+        #region atribute
+        public int coins = 3;
+        #endregion
+
+        #region methode
+        public Player() 
+        {
+            Pile playerCity = new Pile();
+        }
+
+        public void BuyCard()
+        {
+            Pile playerCity = new Pile();
+            playerCity.AddCard()
+
+        }
+
+        public void DiceRolling()
+        {
+
+        }
+        #endregion
     }
 }
