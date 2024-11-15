a3ed1116e77653cc7e2453bd52fa5f8f877c8854
GUIlhem <guilhem.lesaint.marquet@gmail.com>
2024-11-15 13:35:35 +0100

    Card update

--- Minivilles/Card.cs
+++ Minivilles/Card.cs
@@ -8,7 +8,19 @@ namespace Minivilles
 {
     public class Card
     {
+        public int activationNumber;
         public string effect;
         public int price;
+
+        public Card(int _activationNumber, string _effect, int _price) 
+        {
+        
+            activationNumber = _activationNumber;
+            effect = _effect;
+            price = _price;
+        
+        }
+
     }
+
 }
