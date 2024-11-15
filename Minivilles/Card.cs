a566d2c2043298ab9fff9d026167513c9681b484
Tom Foulquier <foulquiertom12@gmail.com>
2024-11-15 13:45:42 +0100

    Cards restore

--- Minivilles/Card.cs
+++ Minivilles/Card.cs
@@ -8,18 +8,17 @@ namespace Minivilles
 {
     public class Card
     {
-
         public int activationNumber;
         public string effect;
         public int price;
 
-        public Card(int _activationNumber, string _effect, int _price) 
+        public Card(int _activationNumber, string _effect, int _price)
         {
-        
+
             activationNumber = _activationNumber;
             effect = _effect;
             price = _price;
-        
+
         }
 
     }
